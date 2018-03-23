765e8a902f9afdcc3995c9881f3e6eaeff0bfc7d
agolomazov <agolomazov@4xxi.com>
2018-03-23 19:09:07 +0300

    Search todos component

--- src/features/todo/todoPage.js
+++ src/features/todo/todoPage.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import styled from 'styled-components';
 import DefaultLayout from '../../hoc/layouts/default';
 import TodoList from './todoList';
+import TodoSearch from './todoSearch';
+
+const SearchContainer = styled.div`
+  margin-bottom: 20px;
+`;
 
 class TodoPage extends Component {
   render() {
     return (
       <DefaultLayout>
         <h1>TodoPage Application</h1>
+        <SearchContainer>
+          <TodoSearch/>
+        </SearchContainer>
         <TodoList/>
       </DefaultLayout>
     );
--- src/features/todo/todoSearch.js
+++ src/features/todo/todoSearch.js
@@ -1,3 +1,25 @@
-/**
- * Created by agolomazov on 22.03.18.
- */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const TodoSearchWrapper = styled.div`
+  border: 1px solid #dedede;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+
+class TodoSearch extends Component {
+  render() {
+    return (
+      <TodoSearchWrapper>
+        <input
+          type="text"
+          placeholder="Search todos..."
+        />
+        <button>Search</button>
+      </TodoSearchWrapper>
+    );
+  }
+};
+
+export default TodoSearch;
